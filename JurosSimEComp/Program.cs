19d8ba96d40d173bb8a016ec3e3195a239eebb2d
Gustavo653 <gustavohs2004@gmail.com>
2021-02-27 10:00:03 -0300

    Captura de eventuais erros na execução do programa

--- JurosSimEComp/Program.cs
+++ JurosSimEComp/Program.cs
@@ -12,51 +12,72 @@ namespace JurosSimEComp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Sistema que calcula juros simples e juros compostos");
-            Console.WriteLine();
-            Console.WriteLine("ATENÇÃO: O APLICATIVO TRABALHA COM JUROS AO MES, E TEMPO EM MESES!");
-            Console.WriteLine("OS JUROS DEVEM SER ESCRITOS DA SEGUINTE FORMA: 2% = 2");
-            Console.WriteLine();
-            Console.Write("Você deseja calcular juros: (Simples / Compostos) ");
-            string strjr = Console.ReadLine();
-            if (strjr == "Compostos")
+            try
             {
-                Console.Write("Insira o capital inicial: ");
-                double capital = double.Parse(Console.ReadLine());
-                Console.Write("Insira a taxa de juros ao mês: ");
-                double taxaDeJuros = double.Parse(Console.ReadLine());
-                Console.Write("Insira o tempo em meses: ");
-                int tempo = int.Parse(Console.ReadLine());
-                JurosCompostos juros = new JurosCompostos(capital, taxaDeJuros, tempo); //Insere as informações no construtor da Classe Juros Simples.
-                double CalcularJuros = juros.CalcularJuros(capital, taxaDeJuros, tempo); //Retorna apenas os juros
-                double ResultadoJuros = juros.CalcularMontante(capital, taxaDeJuros, tempo); //Retorna o valor total
-                Console.WriteLine("Resultados do cálculo: ");
-                Console.Write("O valor dos juros foi de: R$" + CalcularJuros.ToString("F2", CultureInfo.InvariantCulture) + " - ");
-                Console.Write("O valor do capital é de: R$" + capital.ToString("F2", CultureInfo.InvariantCulture) + " - ");
-                Console.Write("O montante total foi de: R$" + ResultadoJuros.ToString("F2", CultureInfo.InvariantCulture));
+
+
+                Console.WriteLine("Sistema que calcula juros simples e juros compostos");
+                Console.WriteLine();
+                Console.WriteLine("ATENÇÃO: O APLICATIVO TRABALHA COM JUROS AO MES, E TEMPO EM MESES!");
+                Console.WriteLine("OS JUROS DEVEM SER ESCRITOS DA SEGUINTE FORMA: 2% = 2");
+                Console.WriteLine();
+                Console.Write("Você deseja calcular juros: (Simples / Compostos) ");
+                string strjr = Console.ReadLine();
+                if (strjr == "Compostos")
+                {
+                    Console.Write("Insira o capital inicial: ");
+                    double capital = double.Parse(Console.ReadLine());
+                    Console.Write("Insira a taxa de juros ao mês: ");
+                    double taxaDeJuros = double.Parse(Console.ReadLine());
+                    Console.Write("Insira o tempo em meses: ");
+                    int tempo = int.Parse(Console.ReadLine());
+                    JurosCompostos juros = new JurosCompostos(capital, taxaDeJuros, tempo); //Insere as informações no construtor da Classe Juros Simples.
+                    double CalcularJuros = juros.CalcularJuros(capital, taxaDeJuros, tempo); //Retorna apenas os juros
+                    double ResultadoJuros = juros.CalcularMontante(capital, taxaDeJuros, tempo); //Retorna o valor total
+                    Console.WriteLine("Resultados do cálculo: ");
+                    Console.Write("O valor dos juros foi de: R$" + CalcularJuros.ToString("F2", CultureInfo.InvariantCulture) + " - ");
+                    Console.Write("O valor do capital é de: R$" + capital.ToString("F2", CultureInfo.InvariantCulture) + " - ");
+                    Console.Write("O montante total foi de: R$" + ResultadoJuros.ToString("F2", CultureInfo.InvariantCulture));
+                }
+                else if (strjr == "Simples")
+                {
+                    Console.Write("Insira o capital inicial: ");
+                    double capital = double.Parse(Console.ReadLine());
+                    Console.Write("Insira a taxa de juros ao mês: ");
+                    double taxaDeJuros = double.Parse(Console.ReadLine());
+                    Console.Write("Insira o tempo em meses: ");
+                    int tempo = int.Parse(Console.ReadLine());
+                    JurosSimples juros = new JurosSimples(capital, taxaDeJuros, tempo); //Insere as informações no construtor da Classe Juros Simples.
+                    double CalcularJuros = juros.CalcularJuros(capital, taxaDeJuros, tempo); //Retorna apenas os juros
+                    double ResultadoJuros = juros.CalcularMontante(capital, taxaDeJuros, tempo); //Retorna o valor total
+                    Console.WriteLine("Resultados do cálculo: ");
+                    Console.Write("O valor dos juros foi de: R$" + CalcularJuros.ToString("F2", CultureInfo.InvariantCulture) + " - ");
+                    Console.Write("O valor do capital é de: R$" + capital.ToString("F2", CultureInfo.InvariantCulture) + " - ");
+                    Console.Write("O montante total foi de: R$" + ResultadoJuros.ToString("F2", CultureInfo.InvariantCulture));
+                }
+                else
+                {
+                    Console.WriteLine("Erro, digite: Simples ou Compostos. Reiniciando programa");
+                    Main(args);
+                }
+                Console.ReadKey();
             }
-            else if (strjr == "Simples")
+            catch (ArgumentNullException e)
             {
-                Console.Write("Insira o capital inicial: ");
-                double capital = double.Parse(Console.ReadLine());
-                Console.Write("Insira a taxa de juros ao mês: ");
-                double taxaDeJuros = double.Parse(Console.ReadLine());
-                Console.Write("Insira o tempo em meses: ");
-                int tempo = int.Parse(Console.ReadLine());
-                JurosSimples juros = new JurosSimples(capital, taxaDeJuros, tempo); //Insere as informações no construtor da Classe Juros Simples.
-                double CalcularJuros = juros.CalcularJuros(capital, taxaDeJuros, tempo); //Retorna apenas os juros
-                double ResultadoJuros = juros.CalcularMontante(capital, taxaDeJuros, tempo); //Retorna o valor total
-                Console.WriteLine("Resultados do cálculo: ");
-                Console.Write("O valor dos juros foi de: R$" + CalcularJuros.ToString("F2", CultureInfo.InvariantCulture) + " - ");
-                Console.Write("O valor do capital é de: R$" + capital.ToString("F2", CultureInfo.InvariantCulture) + " - ");
-                Console.Write("O montante total foi de: R$" + ResultadoJuros.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine(e.Message);
             }
-            else
+            catch (FormatException e)
             {
-                Console.WriteLine("Erro, digite: Simples ou Compostos. Reiniciando programa");
-                Main(args);
+                Console.WriteLine(e.Message);
             }
-            Console.ReadKey();
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
