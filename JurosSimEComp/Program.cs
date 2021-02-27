692f377a0af769f6569a518b4e736e7aa7b33e83
Gustavo653 <gustavohs2004@gmail.com>
2021-02-27 08:13:15 -0300

    Estrutura inicial feita

--- JurosSimEComp/Program.cs
+++ JurosSimEComp/Program.cs
@@ -10,6 +10,27 @@ namespace JurosSimEComp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Sistema que calcula juros simples e juros compostos");
+            Console.WriteLine();
+            Console.WriteLine("ATENÇÃO: O APLICATIVO TRABALHA COM JUROS AO MES, E TEMPO EM MESES!");
+            Console.WriteLine();
+            Console.Write("Você deseja calcular juros: (Simples / Compostos) ");
+            string strjr = Console.ReadLine();
+            strjr.ToLower();
+            if (strjr == "compostos")
+            {
+
+            }
+            else if (strjr == "simples")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Erro, digite novamente. Reiniciando programa");
+                Console.ReadKey();
+                Main(args);
+            }
         }
     }
 }
